802afd2e15e73f9c9d736c21816060ebc258b1ce
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-07-21 23:37:18 +0545

    Add ham-menu for smaller devices

---
+++ script.js
@@ -0,0 +1,9 @@
+function showSideNav() {
+    const sidebar = document.querySelector('.side-nav')
+    sidebar.style.display = 'flex'
+}
+
+function hideSideNav() {
+    const sidebar = document.querySelector('.side-nav')
+    sidebar.style.display = 'none'
+}
