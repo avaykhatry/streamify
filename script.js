3deb2b88e3bc50f0161ad6d3612dc78039afbc91
Avaya <130426359+avaykhatry@users.noreply.github.com>
2025-07-23 00:48:03 +0545

    add toggle option to change theme from classic to noir mode

--- script.js
+++ script.js
@@ -14,8 +14,15 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar-bg").style.background = "#292929";
+        document.getElementById("navbar-bg").style.background = " #292929";
+        document.getElementById("navbar-bg-noir").style.background = "#ccc8ae";
     } else {
         document.getElementById("navbar-bg").style.background = "none";
+        document.getElementById("navbar-bg-noir").style.background = "none";        
     }
+}
+
+function changeTheme() {
+    let element = document.body;
+    element.classList.toggle("noir-mode");
 }
